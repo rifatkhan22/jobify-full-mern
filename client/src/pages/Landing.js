75840f1e49f1a14012d8abb002a065b6eb2ba1e8
khanr97 <tafirnahk@gmail.com>
2023-01-03 21:35:59 -0500

    set link

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,6 +2,7 @@ import React from "react";
 import main from "../assets/images/main-alternative.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -21,7 +22,9 @@ const Landing = () => {
             track time from your browser, or via extension, desktop, and mobile
             app.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="person" className="img main-img" />
       </div>
