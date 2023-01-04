50c7ba28b6100fdfc781dbacb05cd853d3e59100
khanr97 <tafirnahk@gmail.com>
2023-01-03 22:19:27 -0500

    error-page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
-  return <h1>Error</h1>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3>Page was not found. Please try again</h3>
+        <Link to="/">Go back to Dashboard</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
