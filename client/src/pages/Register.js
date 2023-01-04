b44957c754a09270966330be9dd98b812ecfd3ba
khanr97 <tafirnahk@gmail.com>
2023-01-03 23:16:11 -0500

    alert error

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Logo, FormRow, Alert } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
 
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+  showAlert: false,
+};
 const Register = () => {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initialState);
+  //useNavigate and global state
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {values.showAlert && <Alert />}
+        {/* This will be for name input*/}
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* This will be for email input*/}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* This will be for password input*/}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
